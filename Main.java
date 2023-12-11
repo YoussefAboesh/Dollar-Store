a610050a3e6a12474b163f37d66da0a35014b806
Youssef Abo3sh <116193494+YoussefAboesh@users.noreply.github.com>
2023-12-11 02:14:48 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
-public class Main { 
+public class Main {
 
-public static void main(String[] args) {
-        User u = new User();
-        Departments dept = new Departments();
-        Scanner scanner = new Scanner(System.in);
-        u.chooseOne(scanner, dept);  
-        scanner.close();
-    }
+  public static void main(String[] args) {
+    User u = new User();
+    Departments dept = new Departments();
+    Scanner scanner = new Scanner(System.in);
+    u.chooseOne(scanner, dept);
+    scanner.close();
   }
-
-
+}
