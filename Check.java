0e67fd5e59c2f1bdc307c1e2e94e4dea2d48c7f5
Youssef Abo3sh <116193494+YoussefAboesh@users.noreply.github.com>
2023-12-11 02:12:19 +0200

    Update Check.java

--- Check.java
+++ Check.java
@@ -1,126 +1,112 @@
 import java.util.Scanner;
 
-public class Check {
+public class Electronics {
 
-    private String city;
-    private String address;
-    private String phone;
-    private String paymentMethod;
-    private String email;
-    private String password;
-    private String cardnum;
-    private String name;
+    private String[][] ProdByCategory;
+    private int productChoice;
+    private int choice;
+    private Cart cart = new Cart();
 
-    public Check() {
-        this.city = "";
-        this.address = "";
-        this.phone = "";
-        this.paymentMethod = "";
-        this.email = "";
-        this.password = "";
-        this.cardnum = "";
+    public Electronics() {
+        initializeProd();
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Cart getCart() {
+        return cart;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    private void initializeProd() {
+        ProdByCategory = new String[][] {
+                { "Phones and Tablets", "iPhone 14", "Samsung Galaxy Tab", "Redmi Note 9 Pro Max" },
+                { "TVs and Audio Devices", "Sony 4K Smart TV", "LG OLED55CXPUA", "Samsung Neo QLED 8K" },
+                { "Computers and Laptops", "Dell G15", "MacBook M1", "HP Spectre x360" },
+                { "Home Appliances", "Microwave", "Toaster", "Coffee maker" },
+                { "Gaming and Toys", "PlayStation 5", "Xbox Series X", "PlayStation VR" }
+        };
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    public void displayProdByCategory() {
+        String category = "";
+        Scanner input = new Scanner(System.in);
 
-    public void setphone(String phone) {
-        this.phone = phone;
-    }
+        System.out.print(
+                "Categories in DepElectronics:\n1. Phones and Tablets\n2. TVs and Audio Devices\n3. Computers and Laptops\n4. Home Appliances\n5. Gaming and Toys\nSelect a category:");
 
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setCardnum(String cardnum) {
-        this.cardnum = cardnum;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getCardnum() {
-        return cardnum;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void printReceipt() {
-        System.out.println("\nReceipt");
-        System.out.println("Thank you MR." + name);
-        System.out.println("City: " + city);
-        System.out.println("Address: " + address);
-        System.out.println("phone number: " + phone);
-        System.out.println("Payment Method: " + paymentMethod);
-    }
-
-    public void inputData() {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter your city: ");
-        this.setCity(scanner.nextLine());
-
-        System.out.println("Enter your address: ");
-        this.setAddress(scanner.nextLine());
-
-        System.out.println("Enter your phone: ");
-        this.setphone(scanner.nextLine());
-
-        System.out.println("Choose payment method: ");
-        System.out.println("1. Cash");
-        System.out.println("2. PayPal");
-        System.out.println("3. visa");
-        int choice = scanner.nextInt();
-        scanner.nextLine();
+        while (true) {
+            try {
+                choice = input.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println("Error! Please enter a valid number.");
+                input.nextLine();
+            }
+        }
 
         switch (choice) {
             case 1:
-                this.setPaymentMethod("Cash");
+                category = "Phones and Tablets";
                 break;
             case 2:
-                this.setPaymentMethod("PayPal\n");
-                System.out.println("Email: ");
-                this.setEmail(scanner.nextLine());
-                System.out.println("Password: ");
-                this.setPassword(scanner.nextLine());
+                category = "TVs and Audio Devices";
                 break;
             case 3:
-                this.setPaymentMethod("visa");
-                System.out.println("CARD NUM: ");
-                this.setCardnum(scanner.nextLine());
+                category = "Computers and Laptops";
+                break;
+            case 4:
+                category = "Home Appliances";
+                break;
+            case 5:
+                category = "Gaming and Toys";
                 break;
             default:
-                System.out.println("Invalid choice. Defaulting to Cash.");
-                this.setPaymentMethod("Cash");
+                System.out.println("Invalid choice.");
+                displayProdByCategory();
+        }
+
+        for (int i = 0; i < ProdByCategory.length; i++) {
+            String[] prodCategory = ProdByCategory[i];
+            if (prodCategory[0].equals(category)) {
+                System.out.println("\nProducts in " + category + ":");
+                for (int j = 1; j < prodCategory.length; j++) {
+                    System.out.println(j + ". " + prodCategory[j]);
+                }
+                break;
+            }
+        }
+
+        System.out.print("Choose a product:");
+
+        while (true) {
+            try {
+                productChoice = input.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println("Error! Please enter a valid number.");
+                input.nextLine();
+            }
+        }
+
+        if (productChoice > 0 && productChoice <= ProdByCategory[choice - 1].length - 1) {
+            String selectedProduct = ProdByCategory[choice - 1][productChoice];
+            System.out.println("\nYou selected: " + selectedProduct);
+        } else {
+            System.out.println("\nInvalid product choice.");
+        }
+        System.out.println("1. Add to cart\n2. Remove from cart");
+        int cartChoice = input.nextInt();
+        switch (cartChoice) {
+            case 1:
+                cart.addToCart(ProdByCategory[choice - 1][productChoice]);
+                break;
+            case 2:
+                cart.removeFromCart(ProdByCategory[choice - 1][productChoice]);
+                break;
+            default:
+                System.out.println("Invalid choice!");
+                displayProdByCategory();
                 break;
         }
 
-        scanner.close();
-        printReceipt();
+        input.close();
     }
 }
