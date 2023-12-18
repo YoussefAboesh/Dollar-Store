93cd4a11d408e40c55d6ac9706c354c958a40f62
Youssef Abo3sh <116193494+YoussefAboesh@users.noreply.github.com>
2023-12-18 04:38:43 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
+    try {
+      var fileStream = new FileOutputStream("./output-file.txt", true);
+      System.setOut(new PrintStream(fileStream));
+      System.out.println("------------------------------------------");
+    } catch (Exception e) {
+      System.err.println(e);
+    }
     User u = new User();
     Departments dept = new Departments();
     Scanner scanner = new Scanner(System.in);
